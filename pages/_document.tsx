01050ed2c7e73ecc54bc0354e3532530691268a9
elisabethanna <anna.el.eriksson94@gmail.com>
2022-04-16 13:00:29 +0200

    fix: thumbnail

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,14 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
+          <meta property="og:url" content="https://annaerikssons.com/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Anna Eriksson - Portfolio" />
+          <meta property="og:description" content="Anna's personal website." />
+          <meta
+            property="og:image"
+            content="https://annaerikssons.com/thumbnail.png"
+          />
         </Head>
         <body>
           <Main />
